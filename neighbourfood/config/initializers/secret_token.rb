f147c17f99ed7eda6c0591b32a8de275823b9ae2
albertjou <albert_jou@hotmail.com>
2013-10-28 11:11:30 +1100

    First commit

---
+++ neighbourfood/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neighbourfood::Application.config.secret_token = '406be4696bc68889474f43349d6a5afb05d0cc689d252b6bb29254a8c8395a134991debfb9b48b4c872ebe23dde0c32268cd412ab9d8d7d211cc1d68d9a76231'
