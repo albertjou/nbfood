634192bb6d244277e0c379c84b5c56d446e18e1e
albertjou <albert_jou@hotmail.com>
2013-10-29 10:27:50 +1100

    Permissions for meals

--- neighbourfood/app/helpers/application_helper.rb
+++ neighbourfood/app/helpers/application_helper.rb
@@ -2,14 +2,12 @@ module ApplicationHelper
     def intellinav
       links = ""
       if @auth.present?
-        if @auth.is_admin
-          links += "<li>#{link_to("Show Users", users_path)}</li>"
-        end
-        links += "<li>#{ link_to('Edit User', edit_users_path )}</li>"
-        links += "<li>#{ link_to('Logout ' + @authenticated.name, login_path, :method => 'delete', :confirm => 'Are you sure?') }</li>"
+        # if @auth.is_admin
+        #   links += "<li>#{link_to("Show Users", users_path)}</li>"
+        # end
+        links += "<%= render :partial => '/layouts/loggedin' %>"
       else
-        links += "<li>#{ link_to('Create Account', new_user_path) }</li>" +
-        links += "<li>#{ link_to('Login', login_path) }</li>"
+        links += "<%= render :partial => '/session/login' %>"
       end
       links
   end
