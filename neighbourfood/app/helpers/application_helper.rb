843e6fe361e32b4f826686ffa46b4c6451e374ef
albertjou <albert_jou@hotmail.com>
2013-10-29 08:43:48 +1100

    Added meals controller

--- neighbourfood/app/helpers/application_helper.rb
+++ neighbourfood/app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+    def intellinav
+      links = ""
+      if @auth.present?
+        if @auth.is_admin
+          links += "<li>#{link_to("Show Users", users_path)}</li>"
+        end
+        links += "<li>#{ link_to('Edit User', edit_users_path )}</li>"
+        links += "<li>#{ link_to('Logout ' + @authenticated.name, login_path, :method => 'delete', :confirm => 'Are you sure?') }</li>"
+      else
+        links += "<li>#{ link_to('Create Account', new_user_path) }</li>" +
+        links += "<li>#{ link_to('Login', login_path) }</li>"
+      end
+      links
+  end
 end
--- neighbourfood/config/routes.rb
+++ neighbourfood/config/routes.rb
@@ -1,4 +1,14 @@
 Neighbourfood::Application.routes.draw do
+  root :to => 'pages#home'
 
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  get '/logout' => 'session#destroy'
 
+  resources :users, :meals
+  # , except => [:edit] do
+  #   collection do
+  #     get 'edit'
+  #   end
+  # end
 end
