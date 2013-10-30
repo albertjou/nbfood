c6e7ed2a664f2a3c7628d13f36c0db0e512bcd9e
albertjou <albert_jou@hotmail.com>
2013-10-30 23:22:53 +1100

    New meal form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,14 @@ class PagesController < ApplicationController
 def home
   if params[:search].present?
     @meals = Meal.near(params[:search], params[:distance], :order => :distance)
+    if @meals.count == 0
+      @meal_message = true
+      @meals = Meal.all
+    end
   else
     @meals = Meal.all
+    @meal_message = false
   end
-
 end
 
 end
