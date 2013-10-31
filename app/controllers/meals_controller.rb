2da2feb88f562e7759912e0584ee26e50295e958
albertjou <albert_jou@hotmail.com>
2013-11-01 08:10:31 +1100

    Updated homepage

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -55,7 +55,8 @@ class MealsController < ApplicationController
   private
 
   def check_if_logged_in
-    redirect_to root_path unless @auth.present?
+    redirect_to new_user_path unless @auth.present?
+    flash[:notice] = "Almost there, you just need to sign up now or login if you already have an account" unless @auth.present?
   end
 
 end
